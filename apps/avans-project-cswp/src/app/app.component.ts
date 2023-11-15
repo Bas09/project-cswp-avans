91420e8b063980c1d8e29c8a66b027b523a9a15d
Bas09 <basdekker09@gmail.com>
2023-11-15 23:18:33 +0100

    Working application

--- apps/avans-project-cswp/src/app/app.component.ts
+++ apps/avans-project-cswp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FeaturesModule } from '@avans-project-cswp/frontend/features';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FeaturesModule],
   selector: 'avans-project-cswp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- libs/frontend/features/src/index.ts
+++ libs/frontend/features/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/features/features.component';
+// export * from './lib/features.component';
+// export * from './lib/meal/meal-detail/meal-detail.component';
+// export * from './lib/meal/meal-list/meal-list.component';
+// export * from './lib/meal';
+// export * from './lib/meal.module';
+export * from './lib/features.module';
